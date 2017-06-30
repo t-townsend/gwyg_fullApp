f934dc995a95ba3d38c631b9fadc0cbf431d299a
Trista Townsend <tristatownsend@gmail.com>
2017-06-29 21:07:22 -0700

    displayed the two buttons successfully

--- voting-client/client/index.jsx
+++ voting-client/client/index.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Voting from './Voting';
+import Voting from './components/Voting';
 
 const pair = ['Sushi', 'Italian'];
-
-// class Voting extends React.Component {
-//   render() {
-//     return (
-//       <div className="voting">
-//         <h1>I just voted</h1>
-//       </div>
-//     );
-//   }
-// }
 
 ReactDOM.render(
   <Voting pair={pair} />,
   document.getElementById('app')
 );
-
-// ReactDOM.render(
-//   <h1>Hello World</h1>,
-//   document.getElementById('app')
-// );
